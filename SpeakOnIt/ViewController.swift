7b4d4a6a22b8c2316c2f3f898076b776310a5ff6
camposss <ccamposs23@gmail.com>
2018-12-15 12:13:50 -0800

    Initial Commit

---
+++ SpeakOnIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeakOnIt
+//
+//  Created by Mark Lintner on 12/15/18.
+//  Copyright © 2018 Campos Swift Tutorials. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
